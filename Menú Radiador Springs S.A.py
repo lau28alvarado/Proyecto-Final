a82907fe8db419e8d3e19a6f9d52d0d8ed919aa2
lau28alvarado <148724507+lau28alvarado@users.noreply.github.com>
2023-10-24 22:59:49 -0600

    Subida archivo menu
    Entrega primer avance

---
+++ Menú Radiador Springs S.A.py
@@ -0,0 +1,28 @@
+print('Menu')
+print('1. Ver rutas')
+print('2. Ver precios')
+print('3. Adquirir tiquetes')
+print('4. Consultar espacios disponibles')
+print('5. Salir')
+
+opcion = 0 
+
+while opcion <= 4:
+    opcion = int(input("Seleccione la opcion\n"))
+    if opcion == 1:
+        print("Aqui va la opcion 1")
+    elif opcion == 2:
+        print("Aqui va la opcion 2")
+    elif opcion == 3:
+        print("Aqui va la opcion 3" )
+    elif opcion == 4:
+        print("Aqui va la opcion 4" )
+    elif opcion == 5:
+        print("Aqui va la opcion 5" )
+    else:
+        print("Escogio opcion erronea:  ",opcion)
+        print("Digite opcion 1 a 5")
+        opcion = 0
+        
+print('Aqui va la creacion del archivo plano')
+print('Fin del programa')
