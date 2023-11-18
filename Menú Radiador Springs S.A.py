90bb3ad3973bd26fb513daa828a217aeed700fc1
Laura Alvarado <laalher28gmail.com>
2023-11-18 15:56:30 -0600

    Se agregó comentario, def e identación

--- Menú Radiador Springs S.A.py
+++ Menú Radiador Springs S.A.py
@@ -1,20 +1,22 @@
-print('Menu')
-print('1. Ver rutas')
-print('2. Ver precios')
-print('3. Adquirir tiquetes')
-print('4. Consultar espacios disponibles')
-print('5. Salir')
+# SUBPROGRAMA DESPLEGUE EL MENU
+def Desplegue_Menu():
+    print('Menu')
+    print('1. Ver rutas')
+    print('2. Ver precios')
+    print('3. Adquirir tiquetes')
+    print('4. Consultar espacios disponibles')
+    print('5. Salir')
 
 opcion = 0 
 
 while opcion <= 4:
     opcion = int(input("Seleccione la opcion\n"))
     if opcion == 1:
-        print("Aqui va la opcion 1")
+        print("Aqui va la opcion 1") 
     elif opcion == 2:
         print("Aqui va la opcion 2")
     elif opcion == 3:
-        print("Aqui va la opcion 3" )
+        print("Aqui va la opcion 3" ) 
     elif opcion == 4:
         print("Aqui va la opcion 4" )
     elif opcion == 5:
